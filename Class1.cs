67c33741e55fb736779ab1cde89ea5f6a5804982
Jake7299 <62990704+Jake7299@users.noreply.github.com>
2020-04-01 12:59:38 +0300

    Add files via upload
    initial portfilio

---
+++ Class1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Net;
+using System.IO;
+using System.Threading.Tasks;
+using System.Net.Mail;
+
+namespace NetConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            SendEmailAsync().GetAwaiter();
+            Console.Read();
+        }
+
+        private static async Task SendEmailAsync()
+        {
+            MailAddress from = new MailAddress("somemail@gmail.com", "Tom");
+            MailAddress to = new MailAddress("somemail@yandex.ru");
+            MailMessage m = new MailMessage(from, to);
+            m.Subject = "Тест";
+            m.Body = "Письмо-тест 2 работы smtp-клиента";
+            SmtpClient smtp = new SmtpClient("smtp.gmail.com", 587);
+            smtp.Credentials = new NetworkCredential("somemail@gmail.com", "mypassword");
+            smtp.EnableSsl = true;
+            await smtp.SendMailAsync(m);
+            Console.WriteLine("Письмо отправлено");
+        }
+    }
+}
